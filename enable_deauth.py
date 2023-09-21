c724863cd03170b79bdec0a38666252160199ae2
Sniffleupagus <midden_sofas0k@icloud.com>
2023-09-20 17:26:24 -0700

    added touch screen support to toggle attack on and off

--- enable_deauth.py
+++ enable_deauth.py
@@ -41,6 +41,23 @@ class Do_Deauth(plugins.Plugin):
         self._agent = agent
         logging.info("[Enable_Deauth] ready: enabled deauth")
 
+    def on_touch_ready(self, touchscreen):
+        logging.info("[DEAUTH] Touchscreen %s" % repr(touchscreen))
+
+    def on_touch_press(self, ts, ui, ui_element, touch_data):
+        logging.info("[DEAUTH] Touch press: %s" % repr(touch_data));
+        try:
+            if 'point' in touch_data:
+                point = touch_data['point']
+                if point[0] > 50 and point[1] > 190:
+                    logging.info("[DEAUTH] Toggling %s" % repr(self._agent._config['personality']['deauth']))
+                    self._agent._config['personality']['deauth'] = not self._agent._config['personality']['deauth']
+                    uiItems = ui._state._state
+                    uiItems['deauth_count'].label = uiItems['deauth_count'].label.upper() if self._agent._config['personality']['deauth'] else uiItems['deauth_count'].label.lower()
+
+        except Exception as err:
+            logging.info("%s" % repr(err))
+
     def on_deauthentication(self, agent, access_point, client_station):
         self._count += 1
         pass
