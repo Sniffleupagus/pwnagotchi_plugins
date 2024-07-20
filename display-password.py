9772cda6ad3d45c0bd477619e23cd1bc0eec3a67
Pwnagotchi <pwnagotchi@pwnyforge>
2024-07-19 18:25:42 -0700

    bug fixes, match Mac or SSID

--- display-password.py
+++ display-password.py
@@ -1,10 +1,13 @@
-# display-password shows recently cracked passwords on the pwnagotchi display 
+# display-password shows recently cracked passwords of nearby networks
+# on the pwnagotchi display 
 #
 #
 ###############################################################
 #
 # Inspired by, and code shamelessly yoinked from
 # the pwnagotchi memtemp.py plugin by https://github.com/xenDE
+#
+# Modified by Sniffleupagus
 #
 ###############################################################
 from pwnagotchi.ui.components import LabeledValue
@@ -17,27 +20,28 @@ import os
 import random
 
 class DisplayPassword(plugins.Plugin):
-    __author__ = '@nagy_craig'
+    __author__ = '@nagy_craig, Sniffleupagus'
     __version__ = '1.0.0'
     __license__ = 'GPL3'
-    __description__ = 'A plugin to display recently cracked passwords'
+    __description__ = 'A plugin to display recently cracked passwords of nearby networks'
 
 
     def readPotfile(self, fname="/root/handshakes/wpa-sec.cracked.potfile"):
-        st = os.stat(fname)
-        mtime = st.st_mtime if st else 0
+        if os.path.isfile(fname):
+            st = os.stat(fname)
+            mtime = st.st_mtime if st else 0
 
-        if mtime == self.potfile_mtime:
-            logging.debug("Potfile unchanged.")
-        else:
-            logging.info("Potfile changed. Reloading")
-            with open(fname) as f:
-                self.cracked = {}
-                for line in f:
-                    (mac, othermac, ssid, info) = line.strip().split(':', 3)
-                    self.cracked[mac.lower()] = line
-                    self.cracked[ssid.lower()] = line
-    
+            if mtime == self.potfile_mtime:
+                logging.debug("Potfile unchanged.")
+            else:
+                logging.info("Potfile changed. Reloading")
+                with open(fname) as f:
+                    self.cracked = {}
+                    for line in f:
+                        (mac, othermac, ssid, info) = line.strip().split(':', 3)
+                        self.cracked[mac.lower()] = line
+                        self.cracked[ssid.lower()] = line
+
     def __init__(self):
         self.cracked = {}
         self.found = {}
@@ -66,7 +70,7 @@ class DisplayPassword(plugins.Plugin):
             h_pos = (0, 91)
             v_pos = (180, 61)
 
-        if self.options['orientation'] == "vertical":
+        if 'orientation' in self.options and self.options['orientation'] == "vertical":
             ui.add_element('display-password', LabeledValue(color=BLACK, label='', value='',
                                                    position=v_pos,
                                                    label_font=fonts.Bold, text_font=fonts.Small))
@@ -77,8 +81,11 @@ class DisplayPassword(plugins.Plugin):
                                                    label_font=fonts.Bold, text_font=fonts.Small))
 
     def on_unload(self, ui):
-        with ui._lock:
-            ui.remove_element('display-password')
+        try:
+            with ui._lock:
+                ui.remove_element('display-password')
+        except Exception as e:
+            logging.info(e)
 
     # update from list of visible APs, and pick from the file one that matchs
 
@@ -88,13 +95,21 @@ class DisplayPassword(plugins.Plugin):
         
         for ap in access_points:
             mac = ap['mac'].replace(":", "").lower()
-            ssid = ap['hostname'].lower()
+            ssid = ap['hostname'].strip().lower()
             if mac in self.cracked:                
-                logging.debug("AP: %s" % (repr(ap)))
+                logging.info("APmac: %s, %s" % (self.cracked[mac].strip(), repr(ap)))
                 (amac, smac, assid, apass) = self.cracked[mac].strip().split(':', 3)
                 if ssid == assid.lower():
                     logging.debug("Found: %s %s ? %s" % (mac, ssid, self.cracked[mac]))
                     self.found[mac] = assid + ":" + apass                
+            elif ssid in self.cracked:                
+                logging.info("APssid: %s, %s" % (self.cracked[ssid].strip(), repr(ap)))
+                (amac, smac, assid, apass) = self.cracked[ssid].strip().split(':', 3)
+                if ssid == assid.lower():
+                    logging.debug("Found: %s %s ? %s" % (mac, ssid, self.cracked[ssid]))
+                    self.found[mac] = assid + "-" + apass
+            else:
+                logging.info("AP: %s, %s not found" % (mac, ssid))
     
     def on_ui_update(self, ui):
         if len(self.found):
