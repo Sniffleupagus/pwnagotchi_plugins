f39305bdbaf40e2f6bc9e0c9faac5a2f668cd101
Sniffleupagus <midden_sofas0k@icloud.com>
2024-08-01 14:58:06 -0700

    Added web ui to modify personality parameters. Added descriptions of parameters. Displays histogram of avg APs seen on each channel per epoch

--- auto_tune.py
+++ auto_tune.py
@@ -1,5 +1,6 @@
 import logging
 import random
+import time
 
 import pwnagotchi.plugins as plugins
 from pwnagotchi.ui.components import LabeledValue
@@ -7,6 +8,8 @@ from pwnagotchi.ui.view import BLACK
 import pwnagotchi.ui.fonts as fonts
 import pwnagotchi.utils
 
+from flask import abort
+from flask import render_template_string
 
 class auto_tune(plugins.Plugin):
     __author__ = 'sniffleupagus'
@@ -20,13 +23,155 @@ class auto_tune(plugins.Plugin):
         self._active_channels = []
         self._agent = None
 
+        self.descriptions = {
+            "advertise": "enable/disable advertising to mesh peers",
+            "deauth" : "enable/disable deauthentication attacks",
+            "associate" : "enable/disable association attacks",
+            "throttle_a" : "delay after an associate. Some delay seems to reduce nexmon crashes",
+            "throttle_d" : "delay after a deauthenticate. Delay helps reduce nexmon crashes",
+            "assoc_prob" : "probability of trying an associate attack. Set lower to spread out interaction instead of hitting all APs every time until max_interactions",
+            "deauth_prob" : "probability of trying a deauth. will spread the 'max_interactions' over a longer time",
+            "min_rssi" : "ignore APs with signal weaker than this value. lower values will attack more distant APs",
+            "recon_time" : "duration of the bettercap channel hopping scan phase, to discover APs before sending attacks",
+            "min_recon_time" : "time spent on each occupied channel per epoch, sending attacks and waiting for handshakes. and epoch is recon_time + #channels * min_recon_time seconds long",
+            "ap_ttl" : "APs that have not been seen since this many seconds are ignored. Shorten this if you are moving, to not try to scan APs that are no longer in range.",
+            "sta_ttl" : "Clients older than this will ignored",
+        }
+
+
+    def showEditForm(self, request):
+        path = request.path if request.path.endswith("/update") else "%s/update" % request.path
+
+        ret = '<form method=post action="%s">' % path
+        ret += '<input id="csrf_token" name="csrf_token" type="hidden" value="{{ csrf_token() }}">'
+
+        for secname, sec in [["Personality", self._agent._config['personality']]]:
+            ret += '<h2>%s Variables</h2>' % secname
+            ret += '<table>\n'
+            ret += '<tr align=left><th>Parameter</th><th>Value</th><th>Description</th></tr>\n'
+
+            for p in sorted(sec):
+                if type(sec[p]) in [int, str, float, bool]:
+                    cls = type(sec[p]).__name__
+                    ret += "<tr align=left>"
+                    if cls == "bool":
+                        ret += '<th>%s</th><td style="white-space:nowrap; vertical-align:top;">' % (p)
+                        checked = " checked" if sec[p] else ""
+                        ret += 'True:&nbsp;<input type=radio id="newval,%s,%s,%s" name="newval,%s,%s,%s" value="%s" %s>&nbsp;' % (sec[p], p, cls, sec[p], p, cls, "True", checked)
+                        checked = " checked" if not sec[p] else ""
+                        ret += 'False:&nbsp;<input type=radio id="newval,%s,%s,%s" name="newval,%s,%s,%s" value="%s" %s>' % (sec[p], p, cls, sec[p], p, cls, "False", checked)
+                        ret += "</td>"
+                    else:
+                        ret += '<th>%s</th>' % (p)
+                        ret += '<td><input type=text id="newval,%s,%s,%s" name="newval,%s,%s,%s" size="5" value="%s"></td>' % (sec[p], p, cls, sec[p], p, cls, sec[p])
+                        #ret += '<tr><th>%s</th>' % ("" if p not in self.descriptions else self.descriptions[p])
+                    if p in self.descriptions:
+                        ret += "<td>%s</td>" % self.descriptions[p]
+                    ret += '</tr>\n'
+            ret += "</table>"
+        ret += '<input type=submit name=submit value="update"></form><p>'
+        return ret
+
+    def showHistogram(self):
+        ret = ""
+        nloops = int(self._histogram["loops"])
+        if nloops > 0:
+            ret += "<h2>APs per Channel over %s epochs</h2>" % (nloops)
+            ret += "<table>"
+            chans = "<tr>"
+            vals = "<tr>"
+            for (ch, count) in sorted(self._histogram.items(), key=lambda x:x[1], reverse = True):
+                if ch == "loops":
+                    pass
+                else:
+                    count = float(count)/nloops
+                    #ret +="<tr><th>%d</th><td>%0.2f</td>" % (ch, count)
+                    chans += "<th>%s</th>" % ch
+                    vals += "<td>%0.2f</td>" % count
+            chans += "</tr>"
+            vals += "</tr>"
+            ret += chans + vals
+            ret += "</table>"
+        else:
+            ret += "<h2>No channel data collected yet</h2>"
+
+        return ret
+
     # called when http://<host>:<port>/plugins/<plugin>/ is called
     # must return a html page
     # IMPORTANT: If you use "POST"s, add a csrf-token (via csrf_token() and render_template_string)
-    #def on_webhook(self, path, request):
-    #    # display personality parameters for editing
-    #    # show statistic per channel, etc
-    #    pass
+    def on_webhook(self, path, request):
+        # display personality parameters for editing
+        # show statistic per channel, etc
+        if not self._agent:
+            ret = "<html><head><title>AUTO Tune not ready</title></head><body><h1>AUTO Tune not ready</h1></body></html>"
+            return render_template_string(ret)
+
+        try:
+            if request.method == "GET":
+                if path == "/" or not path:
+                    logging.info("webook called")
+                    ret = '<html><head><title>AUTO Tune</title><meta name="csrf_token" content="{{ csrf_token() }}"></head>'
+                    ret += "<body><h1>AUTO Tune</h1><p>"
+                    ret += self.showEditForm(request)
+
+                    ret += self.showHistogram()
+                    ret += "</body></html>"
+                    return render_template_string(ret)
+                # other paths here
+            elif request.method == "POST":
+                ret = '<html><head><title>AUTO Tune</title><meta name="csrf_token" content="{{ csrf_token() }}"></head>'
+                if path == "update": # update settings that changed, save to json file
+                    ret = '<html><head><title>AUTO Tune Update!</title><meta name="csrf_token" content="{{ csrf_token() }}"></head>'
+                    ret += "<body><h1>AUTO Tune Update</h1>"
+                    ret += "<h2>Request</h2><code>"
+                    for (key, val) in request.values.items():
+                        if key != "":
+                            #ret += "%s -> %s<br>\n" % (key,val)
+                            try:
+                                if key.startswith('newval,'):
+                                    (tag, value, parameter, vtype) = key.split(",", 4)
+                                    if parameter in self._agent._config['personality']:
+                                        if val == value:
+                                            pass # ret += "<i>Unchanged %s</i> -> %s<br>" % (key,val)
+                                        elif vtype == "int":
+                                            self._agent._config['personality'][parameter] = int(val)
+                                            ret += "Updated int %s: %s -> %s<br>\n" % (parameter, value, val)
+                                        elif vtype == "float":
+                                            self._agent._config['personality'][parameter] = float(val)
+                                            ret += "Updated float %s: %s -> %s<br>\n" % (parameter, value, val)
+                                        elif vtype == "bool":
+                                            self._agent._config['personality'][parameter] = bool(val == "True")
+                                            ret += "Updated boolean %s: %s -> %s<br>\n" % (parameter, value, val)
+                                        elif vtype == "str":
+                                            self._agent._config['personality'][parameter] = val
+                                            ret += "Updated string %s: %s -> %s<br>\n" % (parameter, value, val)
+                                        else:
+                                            ret += "No update %s (%s): %s -> %s<br>\n" % (parameter, type, value, val)
+                                    else:
+                                        ret += "<b>Skipping unknown %s</b> -> %s<br>" % (key,val)
+                                else:
+                                    pass # ret += "No update %s -> %s<br>\n" % (key, val)
+                            except Exception as e:
+                                ret += "</code><h2>Error</h2><pre>%s</pre><p><code>" % repr(e)
+                                logging.exception(e)
+                    ret += "</code>"
+                    ret += self.showEditForm(request)
+                    ret += self.showHistogram()
+
+                    ret += "</body></html>"
+                else:
+                    ret += "<body><h1>Unknown request</h1>"
+                    ret += '<img src="/ui?%s">' % int(time.time())
+                    ret += "<h2>Path</h2><code>%s</code><p>" % repr(path)
+                    ret += "<h2>Request</h2><code>%s</code><p>" % repr(request.values)
+                    ret += "</body></html>"
+                return render_template_string(ret)
+        except Exception as e:
+            ret = "<html><head><title>AUTO Tune error</title></head>"
+            ret += "<body><h1>%s</h1></body></html>" % repr(e)
+            logging.exception("AUTO Tune error: %s" % repr(e))
+            return render_template_string(ret)
 
     # called when the plugin is loaded
     def on_loaded(self):
