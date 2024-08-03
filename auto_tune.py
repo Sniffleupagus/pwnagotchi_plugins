6072643664c66c8ccd03dd188c959845c88654e9
Sniffleupagus <midden_sofas0k@icloud.com>
2024-08-03 13:26:22 -0700

    Track more statistics per channel and per AP

--- auto_tune.py
+++ auto_tune.py
@@ -1,6 +1,7 @@
 import logging
 import random
 import time
+import html
 
 import pwnagotchi.plugins as plugins
 from pwnagotchi.ui.components import LabeledValue
@@ -13,18 +14,107 @@ from flask import abort
 from flask import render_template_string
 
 class auto_tune(plugins.Plugin):
-    __author__ = 'sniffleupagus'
+    __author__ = 'Sniffleupagus'
     __version__ = '1.0.0'
     __license__ = 'GPL3'
     __description__ = 'A plugin that adjust AUTO mode parameters'
 
-    def __init__(self):
-        self._histogram = {'loops': 0 }
-        self._unscanned_channels = []
-        self._active_channels = []
-        self._agent = None
+    # Chistos - should really be an object, but I'm being lazy
+    #
+    # Channel histograms maintained per session
+    # - stat = string : name of the statistic. It is the chart label for this stat
+    # - channel = the channel where the stat happened
+    # - count (default +1) - how much to add to the count for this chisto[stat][channel]
+    #           for example, on_bcap_wifi_ap_new, use the default value, but on_bcap_wifi_ap_lost use -1
+    #           to count current APs per channel
+    def incrementChisto(self, stat, channel, count=1):
+        if stat not in self._chistos:
+            self._chistos[stat] = { -1: 0}
 
-        self.descriptions = {
+        if channel not in self._chistos[stat]:
+            self._chistos[stat][channel] = count
+        else:
+            self._chistos[stat][channel] += count
+
+        # count all actions per channel, to get a full channel list
+        if channel not in self._chistos['_all_actions']:
+            self._chistos['_all_actions'][channel] = 1
+        else:
+            self._chistos['_all_actions'][channel] += 1
+
+        # track total on channel -1
+        self._chistos[stat][-1] += count
+        self._chistos['_all_actions'][-1] += 1
+
+    def showChistos(self, stats = None, sort_key='_all_actions'): # stats is list of specific to show, else all
+        ret = ""
+        if not stats:
+            stats = self._chistos.keys()
+            logging.info("Using: %s" % repr(stats))
+        try:
+            if sort_key in self._chistos:
+                channel_order = sorted(self._chistos[sort_key].items(), key=lambda x:x[1], reverse = True)
+            else:
+                channel_order = self._agent._supported_channels
+            logging.info("Channel Order: %s" % repr(channel_order))
+
+            ret += "<h2>Channel Statistics</h2>\n"
+            ret += "<table border=1 cellspacing=4 cellpadding=4>\n"
+            ret += "<tr><th>Channel</th>"
+            for (ch, count) in channel_order:
+                if ch == -1:
+                    ret += "<th>All</th>"
+                else:
+                    ret += "<th>%d</th>" % ch
+            ret += "</tr>\n"
+
+            if not stats:
+                ret += "</table>\n"
+                return ret
+
+            for s in stats:
+                if s == sort_key:
+                    ret += "<tr><th>%s</th>" % s
+                else:
+                    ret += "<tr><td>%s</td>" % s
+
+                if s not in self._chistos:
+                    ret += "<td colspan=%s>No data</td>" % len(channel_order)
+                else:
+                    chisto = self._chistos[s]
+                    for (ch, dummy) in channel_order:
+                        if ch in chisto:
+                            ret += "<td align=right>%s</td>" % chisto[ch]
+                        else:
+                            ret += "<td align=center>-</td>"
+                ret += "</tr>\n"
+            ret += "</table>\n"
+
+            return ret
+        except Exception as e:
+            eret = "<h2>Channel Statistics Error</h2>\n"
+            eret += "<h3>Progress:</h3>\n<pre>%s</pre>\n<h3>Exception dump:</h3>\n<pre>%s</pre>\n" % (html.escape(ret), html.escape(repr(e)))
+            logging.exception(e)
+
+    def normalize(self, name):
+        """
+        Only allow alpha/nums
+        """
+        return str.lower(''.join(c for c in name if c.isalnum()))
+
+    def __init__(self):
+        self._histogram = {'loops': 0 } # count APs per epoch
+
+        self._chistos = { '_all_actions' : { -1:0 } }  # arbitrary session stats per channel
+
+        # plugin data
+        self._unscanned_channels = [] # temporary set of channels to pull "extra_channels" from
+        self._active_channels = []    # list of channels with APs found in last scan
+        self._known_aps = {}          # dict of all APs by normalized name+mac
+        self._known_clients = {}      # dict of all clients by normalized APmac+STAmac (many clients to not have names)
+        self._agent = None            # local copy of the pwnagotchi agent, available after on_ready
+
+        self.descriptions = {         # descriptions of personality variables displayed in webui
             "advertise": "enable/disable advertising to mesh peers",
             "deauth" : "enable/disable deauthentication attacks",
             "associate" : "enable/disable association attacks",
@@ -46,6 +136,8 @@ class auto_tune(plugins.Plugin):
         ret = '<form method=post action="%s">' % path
         ret += '<input id="csrf_token" name="csrf_token" type="hidden" value="{{ csrf_token() }}">'
 
+        form_data = request.values.items()
+
         for secname, sec in [["Personality", self._agent._config['personality']],
                              ["AUTO Tune", self._agent._config['main']['plugins']['auto_tune']]]:
             ret += '<h2>%s Variables</h2>' % secname
@@ -55,21 +147,24 @@ class auto_tune(plugins.Plugin):
             for p in sorted(sec):
                 if type(sec[p]) in [int, str, float, bool]:
                     cls = type(sec[p]).__name__
+                    iname = "newval,%s,%s,%s" % (sec[p], p, cls)
                     ret += "<tr align=left>"
                     if cls == "bool":
                         ret += '<th>%s</th><td style="white-space:nowrap; vertical-align:top;">' % (p)
                         checked = " checked" if sec[p] else ""
-                        ret += 'True:&nbsp;<input type=radio id="newval,%s,%s,%s" name="newval,%s,%s,%s" value="%s" %s>&nbsp;' % (sec[p], p, cls, sec[p], p, cls, "True", checked)
+                        ret += '<input type=radio id="%s" name="%s" value="%s" %s>&nbsp;True<br>' % (iname, iname, "True", checked)
                         checked = " checked" if not sec[p] else ""
-                        ret += 'False:&nbsp;<input type=radio id="newval,%s,%s,%s" name="newval,%s,%s,%s" value="%s" %s>' % (sec[p], p, cls, sec[p], p, cls, "False", checked)
+                        ret += '<input type=radio id="%s" name="%s" value="%s" %s>&nbsp;False' % (iname, iname, "False", checked)
                         ret += "</td>"
                     else:
                         ret += '<th>%s</th>' % (p)
-                        ret += '<td><input type=text id="newval,%s,%s,%s" name="newval,%s,%s,%s" size="5" value="%s"></td>' % (sec[p], p, cls, sec[p], p, cls, sec[p])
+                        ret += '<td><input type=text id="%s" name="%s" size="5" value="%s"></td>' % (iname, iname, sec[p])
                         #ret += '<tr><th>%s</th>' % ("" if p not in self.descriptions else self.descriptions[p])
                     if p in self.descriptions:
                         ret += "<td>%s</td>" % self.descriptions[p]
                     ret += '</tr>\n'
+                else:
+                    ret += '<tr align=left><th>%s</th><td>%s</td><td><i>uneditable</i></tr>' % (p, repr(sec[p]))
             ret += "</table>"
         ret += '<input type=submit name=submit value="update"></form><p>'
         return ret
@@ -101,6 +196,32 @@ class auto_tune(plugins.Plugin):
             ret += "</table>"
         else:
             ret += "<h2>No channel data collected yet</h2>"
+
+        return ret
+
+    def showInteractions(self):
+        ret = ""
+        if self._agent:
+            ret += "<h2>Interactions per endpoint</h2>"
+            ret += "<table border=1 spacing=4 cellspacing=4 cellpadding=4>"
+            ret += "<tr><th>Hostname</th><th>MAC</th><th>Encounters</th><th>Associates></th><th>Deauths</th><th>Handshakes</th><th>Interactions</th></tr>"
+            for (id, ap) in sorted(self._known_aps.items(), key=lambda x:x[1]['AT_seen'], reverse=True):
+                lmac = ap['mac'].lower()
+                ret += "<tr><td>%s</td><td>%s</td><td>%s</td>" % (ap['hostname'], ap['mac'], ap['AT_seen'])
+                for t in ['assoc', 'deauth', 'handshake']:
+                    tag = 'AT_' + t
+                    if tag in ap:
+                        ret += "<td>%s</td>" % ap[tag]
+                    else:
+                        ret += "<td></td>"
+                if lmac in self._agent._history:
+                    ret += "<td>%s</td>" % self._agent._history[lmac]
+                else:
+                    ret += "<td>no attacks yet</td>"
+                ret += "</tr>\n"
+#            for (mac, count) in sorted(self._agent._history.items(), key=lambda x:x[1], reverse = True):
+#                ret += "<tr><td>%s</td><td>%s</td><td></td><td>%s</td></tr>" % (mac, mac, count)
+            ret += "</table>\n"
 
         return ret
 
@@ -144,12 +265,15 @@ class auto_tune(plugins.Plugin):
         try:
             if request.method == "GET":
                 if path == "/" or not path:
-                    logging.info("webook called")
+                    logging.debug("webhook called")
                     ret = '<html><head><title>AUTO Tune</title><meta name="csrf_token" content="{{ csrf_token() }}"></head>'
                     ret += "<body><h1>AUTO Tune</h1><p>"
                     ret += self.showEditForm(request)
 
                     ret += self.showHistogram()
+                    ret += self.showChistos()
+                    if 'show_interactions' in self.options and self.options['show_interactions']:
+                        ret += self.showInteractions()
                     ret += "</body></html>"
                     return render_template_string(ret)
                 # other paths here
@@ -191,6 +315,7 @@ class auto_tune(plugins.Plugin):
                         save_config(self._agent._config, "/etc/pwnagotchi/config.toml")
                     ret += self.showEditForm(request)
                     ret += self.showHistogram()
+                    ret += self.showChistos()
                     ret += "</body></html>"
                 else:
                     ret += "<body><h1>Unknown request</h1>"
@@ -211,7 +336,7 @@ class auto_tune(plugins.Plugin):
 
     def on_ready(self, agent):
         self._agent = agent
-
+        self._agent.run("wifi.recon clear")
         if agent._config['ai']['enabled']:
             logging.info("Auto_Tune is inactive when AI is enabled.")
         else:
@@ -225,8 +350,8 @@ class auto_tune(plugins.Plugin):
     def on_wifi_update(self, agent, access_points):
         # check aps and update active channels
         try:
-            if agent._config['ai']['enabled']:
-                return
+            #if agent._config['ai']['enabled']:
+            #    return
 
             active_channels = []
             self._histogram["loops"] = self._histogram["loops"] + 1
@@ -282,4 +407,128 @@ class auto_tune(plugins.Plugin):
             logging.info("Active: %s, Next scan: %s, yet unscanned: %d %s" % (self._active_channels, next_channels, len(self._unscanned_channels), self._unscanned_channels))
         except Exception as e:
             logging.exception(e)
+
+    def markAPSeen(self, access_point, context=None):
+        try:
+            apname = self.normalize(access_point['hostname'])
+            apmac = self.normalize(access_point['mac'])
+            apID = apname + '-' + apmac
+            channel = access_point['channel']
+
+            contextlabel = "on "+context if context else ""
+            tag = 'AT_' + context if context else 'AT_seen'
             
+            if apID not in self._known_aps:
+                # first time seen this AP
+                self._known_aps[apID] = access_point.copy()
+                self._known_aps[apID]['AT_seen'] = 1
+                self._known_aps[apID][tag] = 1
+                self._known_aps[apID]['AT_visible'] = True
+
+                self.incrementChisto('Unique APs', channel)
+                self.incrementChisto('Current APs', channel)
+
+                logging.info("New AP%s: %s" % (contextlabel, apID))
+            else:
+                # seen before, merge info
+                for p in access_point:
+                    self._known_aps[apID][p] = access_point[p]
+
+                # if wasn't visible, increment current count
+                if self._known_aps[apID]['AT_visible'] == False:
+                    self._known_aps[apID]['AT_visible'] = True
+                    self.incrementChisto('Current APs', channel)
+
+                # increment context count in the AP data
+                self._known_aps[apID][tag] = 1 if tag not in self._known_aps[apID] else self._known_aps[apID][tag]+1
+                if not context:
+                    logging.info("Returning AP: %s" % (apID))
+
+            return True
+        except Exception as e:
+            logging.exception(e)
+            return False
+
+    # called when the agent is sending an association frame
+    def on_association(self, agent, access_point):
+        try:
+            self.incrementChisto('Associations', access_point['channel'])
+            self.markAPSeen(access_point, "assoc")
+
+        except Exception as e:
+            logging.exception(e)
+
+    # called when the agent is deauthenticating a client station from an AP
+    def on_deauthentication(self, agent, access_point, client_station):
+        try:
+            self.incrementChisto('Deauths', access_point['channel'])
+            self.markAPSeen(access_point, "deauth")
+
+        except Exception as e:
+            logging.exception(e)
+
+    # callend when the agent is tuning on a specific channel
+    def on_channel_hop(self, agent, channel):
+        pass
+
+    # called when a new handshake is captured, access_point and client_station are json objects
+    # if the agent could match the BSSIDs to the current list, otherwise they are just the strings of the BSSIDs
+    def on_handshake(self, agent, filename, access_point, client_station):
+        try:
+            self.incrementChisto('Handshakes', access_point['channel'])
+            self.markAPSeen(access_point, "handshake")
+        except Exception as e:
+            logging.exception(e)
+
+    def on_bcap_wifi_ap_new(self, agent, event):
+        try:
+            ap = event['data']
+            apname = self.normalize(ap['hostname'])
+            apmac = self.normalize(ap['mac'])
+            apID = apname + '-' + apmac
+            channel = ap['channel']
+
+            self.markAPSeen(ap)
+
+        except Exception as e:
+            logging.exception(repr(e))
+
+    def on_bcap_wifi_ap_lost(self, agent, event):
+        try:
+            ap = event['data']
+            apname = self.normalize(ap['hostname'])
+            apmac = self.normalize(ap['mac'])
+            apID = apname + '-' + apmac
+            channel = ap['channel']
+
+            if apID not in self._known_aps:
+                self.incrementChisto('Missed joins', channel)
+                logging.warn("Unknown AP '%s' seen leaving" % apID)
+            else:
+                if self._known_aps[apID]['AT_visible'] == False:
+                    self.incrementChisto('Missed rejoins', channel)
+                    logging.warn("AP '%s' already gone", apID)
+                else:
+                    self._known_aps[apID]['AT_visible'] = False
+                    self.incrementChisto('Current APs', channel, -1)
+
+        except Exception as e:
+            logging.exception(repr(e))
+
+    def on_bcap_wifi_client_new(self, agent, event):
+        try:
+            ap = event['data']['AP']
+            cl = event['data']['Client']
+            apmac = self.normalize(ap['mac'])
+            clmac = self.normalize(cl['mac'])
+            clID = clmac + '-' + apmac
+            channel = ap['channel']
+        except Exception as e:
+            logging.exception(repr(e))
+
+    def on_bcap_wifi_client_lost(self, agent, event):
+        try:
+            ap = event['data']['AP']
+            cl = event['data']['Client']
+        except Exception as e:
+            logging.exception(repr(e))
